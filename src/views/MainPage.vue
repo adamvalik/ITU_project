1bb6344e6039bb637fae54b5ffaadc001ec206b6
adamvalik <xvalik05@stud.fit.vutbr.cz>
2024-10-24 10:44:43 +0200

    bg music

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,12 +1,16 @@
 <template>
   <div @click="playMusic" class="relative flex flex-col justify-center items-center min-h-screen bg-cover bg-center" style="background-image: url('/assets/bg.png');">
     
-    <!-- Multiple Clouds Moving Across the Background -->
+    <!-- clouds -->
     <img v-for="(cloud, index) in clouds" :key="index" :src="cloud.src" :class="cloud.class" @click="playButtonSound" :style="getCloudStyle(index)" />
 
-    <!-- Game Title -->
-    <h1 class="text-7xl font-bold mb-8 z-10">TANKS</h1>
-
+    <div>
+      <h1 class="relative text-7xl font-bold mb-8 z-10">
+        TANKS
+        <p class="absolute -bottom-4 -right-6 text-xl">1v1</p>
+      </h1>
+    </div>
+    
     <!-- Language Flags -->
     <img src="/assets/czech-flag.png" alt="cz" class="w-8 h-8 absolute right-16 top-4 z-10" />
     <img src="/assets/uk-flag.png" alt="uk" class="w-8 h-8 absolute right-4 top-4 z-10" />
@@ -16,12 +20,12 @@
       <button @click="showGameModes" class="border-4 border-sky-700 text-center bg-sky-300 hover:bg-sky-400 font-bold text-xl py-4 px-32 rounded-2xl">
         START GAME
       </button>
-      <RouterLink to="/tutorial" class="border-4 border-sky-700 text-center bg-sky-300 hover:bg-sky-400 font-bold text-xl py-4 px-32 rounded-2xl">
+      <router-link to="/tutorial" class="border-4 border-sky-700 text-center bg-sky-300 hover:bg-sky-400 font-bold text-xl py-4 px-32 rounded-2xl">
         TUTORIAL
-      </RouterLink>
-      <RouterLink to="/createMap" class="border-4 border-sky-700 text-center bg-sky-300 hover:bg-sky-400 font-bold text-xl py-4 px-32 rounded-2xl">
+      </router-link>
+      <router-link to="/createMap" class="border-4 border-sky-700 text-center bg-sky-300 hover:bg-sky-400 font-bold text-xl py-4 px-32 rounded-2xl">
         CREATE MAP
-      </RouterLink>
+      </router-link>
       <button @click="showSettings" class="border-4 border-sky-700 text-center bg-sky-300 hover:bg-sky-400 font-bold text-xl py-4 px-32 rounded-2xl">
         SETTINGS
       </button>
@@ -29,72 +33,40 @@
 
     <!-- Game Modes Modal -->
     <div v-if="isGameModesVisible" @click="hideGameModes" class="fixed inset-0 flex items-center justify-center gap-20 bg-black bg-opacity-50 z-50">
-      <RouterLink @click.stop to="/chooseTanks" class="h-200 text-2xl font-bold bg-white p-8 rounded-lg shadow-2xl hover:bg-gray-100 w-96 h-48 flex items-center justify-center">
+      <router-link @click.stop to="/chooseTanks" class="h-200 text-2xl font-bold bg-white p-8 rounded-lg shadow-2xl hover:bg-gray-100 w-96 h-48 flex items-center justify-center">
         Classic mode
-      </RouterLink>
-      <RouterLink @click.stop to="/chooseTanks" class="h-200 text-2xl font-bold bg-white p-8 rounded-lg shadow-2xl hover:bg-gray-100 w-96 h-48 flex items-center justify-center">
+      </router-link>
+      <router-link @click.stop to="/chooseTanks" class="h-200 text-2xl font-bold bg-white p-8 rounded-lg shadow-2xl hover:bg-gray-100 w-96 h-48 flex items-center justify-center">
         Custom mode
-      </RouterLink>
+      </router-link>
     </div>
 
     <!-- Modal Window -->
     <div v-if="isSettingsVisible" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div class="bg-white p-8 rounded-lg shadow-lg w-96 text-center">
-        <h2 class="text-2xl font-bold mb-4">Settings</h2>
-
-        <!-- Sound Effects Slider -->
-        <div class="mb-4">
-          <label class="font-semibold text-lg">Sound Effects: {{ soundEffectsVolume }}%</label>
-          <input 
-            type="range" 
-            min="0" 
-            max="100" 
-            v-model="soundEffectsVolume"
-            class="w-full mt-2"
-          >
-        </div>
-
-        <!-- Music Volume Slider -->
-        <div class="mb-4">
-          <label class="font-semibold text-lg">Music Volume: {{ musicVolume }}%</label>
-          <input 
-            type="range" 
-            min="0" 
-            max="100" 
-            v-model="musicVolume"
-            class="w-full mt-2"
-          >
-        </div>
-
-        <!-- Close button -->
-        <button @click="hideSettings" class="mt-8 px-6 py-2 bg-gray-500 hover:bg-gray-600 text-white rounded-lg">
-          Close
-        </button>
-      </div>
+      <Settings @hideSettings="hideSettings" @updateMusicVolume="updateMusicVolume" />
     </div>
 
     <!-- Audio Element for Button Click Sound -->
-    <audio ref="buttonSound" :src="buttonSoundSrc" preload="auto"></audio>
-
-    <!-- Audio Element (Invisible) -->
-    <audio ref="backgroundMusic" autoplay loop>
-      <source src="../assets/music.mp3" type="audio/mpeg">
-    </audio>
+    <!-- <audio ref="buttonSound" :src="buttonSoundSrc" preload="auto"></audio> -->
   </div>
 
   <footer class="absolute bottom-4 left-4 text-gray-700 text-sm z-10">© Rogalo 2024</footer>
 </template>
 
 <script>
+import Settings from '../components/Settings/Settings.vue';
+
 export default {
   name: 'MainPage',
+  components: {
+    Settings,
+  },
   data() {
     return {
       isSettingsVisible: false,   // Controls whether the modal is visible
       isGameModesVisible: false,  // Controls whether the game modes modal is visible
-      soundEffectsVolume: 50,     // Default value for sound effects slider
-      musicVolume: 0,            // Default value for music slider
-      buttonSoundSrc: "/assets/another-one.mp3",  // Path to button click sound
+      // soundEffectsVolume: 50,     // Default value for sound effects slider
+      // buttonSoundSrc: "/assets/another-one.mp3",  // Path to button click sound
       // Array of cloud properties for dynamic rendering
       clouds: [
         { src: '/assets/cloud.png', class: 'cloud', style: 'top: 70px; animation-duration: 20s;' },
@@ -110,30 +82,23 @@ export default {
       ],
     };
   },
-  mounted() {
-    // Set initial volume when component mounts
-    this.$refs.backgroundMusic.volume = this.musicVolume / 100;
-  },
-  watch: {
-    // Watch for changes to the music volume slider and update audio element volume
-    musicVolume(newValue) {
-      this.$refs.backgroundMusic.volume = newValue / 100;
-    },
-    // Watch for changes to the sound effects volume slider and update button sound volume
-    soundEffectsVolume(newValue) {
-      this.$refs.buttonSound.volume = newValue / 100;
-    }
-  },
   methods: {
     // Play button sound effect
-    playButtonSound() {
-      this.$refs.buttonSound.currentTime = 0; // Reset sound to start
-      this.$refs.buttonSound.play();
-    },
+    // playButtonSound() {
+    //   this.$refs.buttonSound.currentTime = 0; // Reset sound to start
+    //   this.$refs.buttonSound.play();
+    // },
     // Play background music
     playMusic() {
-      this.$refs.backgroundMusic.play();
+      this.$emit('playMusic'); 
     },
+    updateMusicVolume(volume) {
+      this.$emit('updateMusicVolume', volume);
+    },
+
+
+
+
     // Show the settings modal
     showSettings() {
       this.isSettingsVisible = true;

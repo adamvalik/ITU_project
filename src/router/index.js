32b7296dd49d4c743afaf45f2f5d36fe5f731bde
marek <marekeff@seznam.cz>
2024-10-24 10:28:35 +0200

    AAAAsateceecec

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import MainPage from '../views/MainPage.vue'
 import ChooseTankView from '@/views/ChooseTankView.vue';
 import ChooseMapView from '@/views/ChooseMapView.vue';
+import MapCreator from "@/views/MapCreator.vue";
 
 
 const routes = [
   { path: '/', component: MainPage },  
   { path: '/chooseTanks', component: ChooseTankView },  
-  { path: '/chooseMap', component: ChooseMapView }      
+  { path: '/chooseMap', component: ChooseMapView },
+  { path: '/mapCreator', component: MapCreator }
 
   // { path: '/game', component: GamePage },      
 ];

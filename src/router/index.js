76199850e2ca2fc4d29a06d69096d8f3720934a0
Dominik Horut <xhorut01@vutbr.cz>
2024-10-24 09:04:26 +0200

    fix: updated paths

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,14 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import MainPage from '../views/MainPage.vue'
 import ChooseTankView from '@/views/ChooseTankView.vue';
 import ChooseMapView from '@/views/ChooseMapView.vue';
+import ShopView from '@/views/ShopView.vue';
 
 
 const routes = [
   { path: '/', component: MainPage },  
   { path: '/chooseTanks', component: ChooseTankView },  
-  { path: '/chooseMap', component: ChooseMapView }      
+  { path: '/chooseMap', component: ChooseMapView }, 
+  { path: '/shop', component: ShopView}     
 
   // { path: '/game', component: GamePage },      
 ];

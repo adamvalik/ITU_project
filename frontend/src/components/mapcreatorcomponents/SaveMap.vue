61799ba2513dada503e8d8eec73d29d9ef930a02
marek <marekeff@seznam.cz>
2024-11-13 08:45:48 +0100

    blbustka2

--- frontend/src/components/mapcreatorcomponents/SaveMap.vue
+++ frontend/src/components/mapcreatorcomponents/SaveMap.vue
@@ -2,7 +2,7 @@
   <div v-if="visible" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center" @click="handleOverlayClick">
     <div class="bg-white p-6 rounded-lg shadow-lg w-96" @click.stop>
       <h2 class="text-2xl font-bold mb-4">Save Map</h2>
-      <label for="mapName" class="block text-lg mb-2">Maap Name:</label>
+      <label for="mapName" class="block text-lg mb-2">Map Name:</label>
       <input v-model="mapName" id="mapName" type="text" class="w-full p-2 border border-gray-300 rounded mb-4" />
       <div class="flex justify-between">
         <button @click="saveMap" class="bg-green-300 hover:bg-green-400 text-green-700 font-bold py-2 px-4 rounded">
